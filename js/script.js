51d03e1384089a9ad6481d454ae51cb91dc1ec8d
Margaritf <m.rogdetvenskaya@mail.ru>
2018-09-19 00:15:19 +0300

    Правки после первой проверки

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@
 var writeUsButton = document.querySelector('.write-us-button');
 var writeUsPopup = document.querySelector('.modal-write-us');
 var close = writeUsPopup.querySelector('.modal-close');
+var form = writeUsPopup.querySelector('form');
+var email = writeUsPopup.querySelector('[name=email]');
+var text = writeUsPopup.querySelector('[name=input-text]');
 
 var openPopup = function () {
 	writeUsPopup.classList.add('modal-show');
@@ -20,4 +23,11 @@ writeUsButton.addEventListener('click', function (evt) {
 close.addEventListener('click', function (evt) {
 	evt.preventDefault();
   closePopup();
+});
+
+form.addEventListener('submit', function (evt) {
+	if (!text.value || !email.value) {
+    evt.preventDefault();
+		writeUsPopup.classList.remove('modal-error');
+	}
 });
